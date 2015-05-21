219532ad099b406575f022cbcdedeb35fd281823
Abigail Watson <awatson1978@gmail.com>
2015-05-21 10:51:50 -0500

    glass-ui package style sheet

--- examples/kentucky-derby/client/app/components/activeFoo/globalHelpers.js
+++ examples/kentucky-derby/client/app/components/activeFoo/globalHelpers.js
@@ -1,3 +1,17 @@
 Template.registerHelper("getFooSearchFilter", function(argument){
   return Session.get("fooSearchFilter");
 });
+
+Template.appLayout.layout = function(){
+    Session.set('transparencyDivHeight', $('#innerPanel').height() + 40);
+    if(Session.get('appWidth') > 768){
+      Session.set('transparencyDivLeft', (Session.get('appWidth') - 768) * 0.5);
+    }else{
+      Session.set('transparencyDivLeft', 0);
+    }
+}
+Template.appLayout.delayedLayout = function(timeout){
+  Meteor.setTimeout(function(){
+    Template.appLayout.layout();
+  }, timeout);
+}
--- packages/glass-ui/package.js
+++ packages/glass-ui/package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: '',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'http://github.com/awatson1978/meteor-cookbook/packages/glass-ui',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -12,7 +12,8 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('glass-ui.js');
+  api.use("less");
+  api.addFiles('glass-ui.less');
 });
 
 Package.onTest(function(api) {
