af382d80ec09179d3c100cc57080614d0d188095
Abigail Watson <awatson1978@gmail.com>
2015-07-31 20:07:27 -0500

    updated glass-ui readme

--- packages/starrynight-glass-ui/package.js
+++ packages/starrynight-glass-ui/package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: "Inspired by the Day Made of Glass videos....",
   // URL to the Git repository containing the source code for this package.
-  git: "http://github.com/awatson1978/meteor-cookbook/packages/glass-ui",
+  git: "http://github.com/awatson1978/meteor-cookbook/packages/starynight-glass-ui",
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: "README.md"
